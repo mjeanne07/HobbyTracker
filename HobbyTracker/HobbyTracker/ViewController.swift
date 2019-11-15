0fe0e16cdf1fe5b983f793c39690f08a891e3f4c
morgan smith <mssmitty07@gmail.com>
2019-11-14 21:40:36 -0500

    initial commit

---
+++ HobbyTracker/HobbyTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HobbyTracker
+//
+//  Created by Morgan Smith on 11/14/19.
+//  Copyright © 2019 Morgan Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
